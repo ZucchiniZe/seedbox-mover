fd98e53894d6aedd628ec88d0377757019036f24
Alex Bierwagen <me@alexb.io>
2020-06-03 12:44:54 -0700

    add some pythonn automation

---
+++ noxfile.py
@@ -0,0 +1,35 @@
+import tempfile
+
+import nox
+
+locations = "src", "noxfile.py"
+nox.options.sessions = "lint", "mypy"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+@nox.session
+def lint(session):
+    args = session.posargs or locations
+    install_with_constraints(
+        session, "flake8", "flake8-bandit", "flake8-black", "flake8-import-order"
+    )
+    session.run("flake8", *args)
+
+
+@nox.session
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
