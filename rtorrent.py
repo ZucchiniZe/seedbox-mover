2ea7efa8b43d4f900daba3e257530d41c3a72d4b
Alex Bierwagen <me@alexb.io>
2020-06-02 21:55:41 -0700

    make datetime optional due to torrents being unfinished

--- rtorrent.py
+++ rtorrent.py
@@ -1,7 +1,7 @@
-from datetime import datetime
-from dataclasses import dataclass
-from typing import List, Dict, Any
 import xmlrpc.client
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List, Optional
 
 server_url = "***REMOVED***"
 server = xmlrpc.client.Server(server_url)
@@ -15,11 +15,11 @@ class Torrent:
     ratio: float
     label: str
     added: datetime
-    finished: datetime
+    finished: Optional[datetime]
     trackers: List[str]
 
 
-def get_torrents() -> List[Torrent]:
+def get_all_torrents() -> List[Torrent]:
     """
     Calls rTorrent XMLRPC endpoint with a multicall to get a list of torrrents
 
@@ -38,14 +38,22 @@ def get_torrents() -> List[Torrent]:
                                )
 
     for torrent in list:
+        if torrent[5]:
+            finished = datetime.fromtimestamp(torrent[5])
+        else:
+            finished = None
         torrents.append(Torrent(
             hash=torrent[0],
             name=torrent[1],
             ratio=torrent[2] / 1000,
             label=torrent[3],
             added=datetime.fromtimestamp(torrent[4]),
-            finished=datetime.fromtimestamp(torrent[5]),
+            finished=finished,
             trackers=[group[1] for group in torrent[6]]
         ))
 
     return torrents
+
+
+def get_finished_torrents() -> List[Torrent]:
+    return filter(lambda torrent: torrent.finished, get_all_torrents())
