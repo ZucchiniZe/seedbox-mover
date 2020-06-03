8b6b834bb4e3af7c2910b8e7080e2231868a26f8
Alex Bierwagen <me@alexb.io>
2020-06-02 23:45:49 -0700

    add docs

--- radarr.py
+++ radarr.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 import requests
 
 BASE_URL = '***REMOVED***'
@@ -5,10 +7,15 @@ API_KEY = '***REMOVED***'
 
 
 def _url(path: str) -> str:
+    '''Helper function to build url with apikey at the end'''
     return f'{BASE_URL}{path}?apiKey={API_KEY}'
 
 
-def build_db():
+def get_movie_filepaths() -> Dict[str, Dict[str, str]]:
+    '''
+    Calls Radarr API to and builds a dict with the key being the original torrent name
+    and the value being a dict to build the renamed file path
+    '''
     r = requests.get(_url('/movie'))
     movies = {}
 
